f4e19bc34503eb065886c2fe7718c762aacc2207
Michael Turquette <mturquette@deferred.io>
2016-06-23 20:38:25 -0700

    offlineimap.py: adjust path for homebrew
    Signed-off-by: Michael Turquette <mturquette@deferred.io>

--- .local/bin/offlineimap.py
+++ .local/bin/offlineimap.py
@@ -2,6 +2,6 @@
 import re, subprocess
 
 def decrypt_passwd(account=None):
-    command = "/usr/bin/pass %s" % account
+    command = "/usr/local/bin/pass %s" % account
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
     return output
