82cf0e468ab3ccf550b89958755e5fc7d76618ec
Michael Turquette <mturquette@deferred.io>
2018-03-01 12:08:15 -0800

    the great afew / alot / offlineimap purging
    Only thing still needed from ~/.local/bin is msmtpq and msmtp-queue. the
    latter should just be a function/alias in oh-my-fish to
    /usr/local/share/msmtp/scripts/...
    
    Signed-off-by: Michael Turquette <mturquette@deferred.io>

--- .local/bin/offlineimap.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import re, subprocess
-
-def decrypt_passwd(account=None):
-    command = "/usr/local/bin/pass %s" % account
-    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    return output
